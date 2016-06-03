09daa893b3e56c8876ed09e9c87edbfdd5ee7cba
Garen Torikian <gjtorikian@gmail.com>
2016-06-03 16:10:10 -0700

    Actually block content

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
+function iframeRef(frameRef) {
+  return frameRef.contentWindow
+      ? frameRef.contentWindow.document
+      : frameRef.contentDocument;
+}
+
+function pleaseDoTrack() {
+  return navigator.doNotTrack != 1
+}
+
 export function initialize() {
-  if(navigator.doNotTrack != 1) {
+  if (pleaseDoTrack()) {
     return;
   }
+  const BANNED_LIST = [/youtube.com/];
 
-  const BANNED_LIST = [/youtube.com/]
-  let loaded = {}
-
-  let iFrames = document.getElementsByTagName("iFrame");
+  let iFrames = window.frames;
   for (let i = 0; i < iFrames.length; i++) {
-    iFrames[i].onload = function(event) {
-      let iFrame = event.srcElement;
-      BANNED_LIST.forEach(function(domain) {
-        if (domain.test(iFrame.getAttribute("src"))) {
-          // var newDiv = document.createElement('div');
-          // newDiv.setAttribute("class", "iframe-dnt-container");
-          iFrame.parentNode.setAttribute("class", "iframe-dnt-container");
-          // newDiv.appendChild(iFrame);
+    let iFrame = iFrames[i].frameElement;
+    BANNED_LIST.forEach(function(domain) {
+      if (domain.test(iFrame.src)) {
+        iFrames[i].stop();
 
-          // let iFrameContents = iFrame.innerHTML;
-          // let wrappediFrame = `<div class="iFrame-dnt-container">${iFrame}</div>`;
-          // iFrame.innerHTML = wrappediFrame;
-        }
-      });
-    }
+        let head = iframeRef(iFrame).getElementsByTagName("head")[0];
+        let body = iframeRef(iFrame).getElementsByTagName("body")[0];
+
+        let styleTag = document.createElement("style");
+        styleTag.innerText = ".dnt-back{position:absolute;top:0;bottom:0;left:0;right:0;background-color:rgba(70,70,70,.7)}.dnt-warning{width:80%;position:absolute;left:50%;top:35%;margin:-10% 0 0 -40%;text-align:center}.dnt-warning:after{content:\"Sorry! Looks like this video does not respect DNT!\"}";
+        head.appendChild(styleTag);
+
+        // replace contents of iframe
+        let contentWrapper = document.createElement("div");
+        contentWrapper.setAttribute("class", "dnt-back");
+        let warningDiv = document.createElement("div");
+        warningDiv.setAttribute("class", "dnt-warning");
+
+        contentWrapper.appendChild(warningDiv);
+        body.appendChild(contentWrapper);
+      }
+    });
   }
 };
 
